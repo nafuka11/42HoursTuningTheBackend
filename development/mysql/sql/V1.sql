20c63209ff5e2d525064235d3fed0089a9b440d0
nfukada <nfukada@student.42tokyo.jp>
2022-04-17 01:50:45 +0900

    feat(mysql): record_last_accessにindex追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -2,3 +2,4 @@ ALTER TABLE session ADD INDEX index_value (value);
 ALTER TABLE record ADD INDEX index_order (updated_at DESC, record_id ASC);
 ALTER TABLE record_comment ADD INDEX index_linked_record_id (linked_record_id);
 ALTER TABLE record_item_file ADD INDEX index_linked_record_id (linked_record_id);
+ALTER TABLE record_last_access ADD INDEX index_user_record (user_id, record_id);
