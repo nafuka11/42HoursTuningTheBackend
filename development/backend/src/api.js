25fb24861f9a0b2e193bb26fac4dbfb5b1db746e
nfukada <nfukada@student.42tokyo.jp>
2022-04-17 00:06:52 +0900

    feat(backend): GET /records/{recordId}/commentでなるべくJOINするように

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -751,15 +751,22 @@ const getComments = async (req, res) => {
 
   const recordId = req.params.recordId;
 
-  const commentQs = `select * from record_comment where linked_record_id = ? order by created_at desc`;
+  const commentQs = `select record_comment.comment_id,\
+ record_comment.value,\
+ record_comment.created_by,\
+ record_comment.created_at,\
+ group_member.group_id,\
+ user.name\
+ from record_comment\
+ join group_member on group_member.user_id = record_comment.created_by\
+ join user on user.user_id = record_comment.created_by\
+ where linked_record_id = ? order by created_at desc;`
 
   const [commentResult] = await pool.query(commentQs, [`${recordId}`]);
   mylog(commentResult);
 
   const commentList = Array(commentResult.length);
 
-  const searchPrimaryGroupQs = `select * from group_member where user_id = ? and is_primary = true`;
-  const searchUserQs = `select * from user where user_id = ?`;
   const searchGroupQs = `select * from group_info where group_id = ?`;
   for (let i = 0; i < commentResult.length; i++) {
     let commentInfo = {
@@ -772,25 +779,18 @@ const getComments = async (req, res) => {
     };
     const line = commentResult[i];
 
-    const [primaryResult] = await pool.query(searchPrimaryGroupQs, [line.created_by]);
-    if (primaryResult.length === 1) {
-      const primaryGroupId = primaryResult[0].group_id;
+    commentInfo.commentId = line.comment_id;
+    commentInfo.value = line.value;
+    commentInfo.createdBy = line.created_by;
+    commentInfo.createdAt = line.created_at;
+    commentInfo.createdByName = line.name;
 
+    if (line.group_id) {
       const [groupResult] = await pool.query(searchGroupQs, [primaryGroupId]);
       if (groupResult.length === 1) {
         commentInfo.createdByPrimaryGroupName = groupResult[0].name;
       }
     }
-
-    const [userResult] = await pool.query(searchUserQs, [line.created_by]);
-    if (userResult.length === 1) {
-      commentInfo.createdByName = userResult[0].name;
-    }
-
-    commentInfo.commentId = line.comment_id;
-    commentInfo.value = line.value;
-    commentInfo.createdBy = line.created_by;
-    commentInfo.createdAt = line.created_at;
 
     commentList[i] = commentInfo;
   }
