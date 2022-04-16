f71db2f416d2e521910dd48be341d8d310f7b15d
nfukada <nfukada@student.42tokyo.jp>
2022-04-16 21:57:09 +0900

    feat(mysql): record_comment, record_item_fileにindex追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,4 @@
 ALTER TABLE session ADD INDEX index_value (value);
 ALTER TABLE record ADD INDEX index_order (updated_at DESC, record_id ASC);
+ALTER TABLE record_comment ADD INDEX index_linked_record_id (linked_record_id);
+ALTER TABLE record_item_file ADD INDEX index_linked_record_id (linked_record_id);
